a78bceda4a2da665a6380defebe69f1ff3b8b133
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-16 22:39:11 +0530

    add getter and setter functions

--- src/User.java
+++ src/User.java
@@ -12,4 +12,40 @@ public class User {
         this.phoneNumber = phoneNumber;
         this.password = password;
     }
+
+
+    public int getUserId() {
+        return userId;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
 }
