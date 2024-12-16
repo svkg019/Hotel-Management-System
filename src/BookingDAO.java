5b42027b122135a9de7280715f3426546de0a726
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-16 22:35:15 +0530

    add BookingDAO class with necessary DS and functions

--- src/BookingDAO.java
+++ src/BookingDAO.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookingDAO {
+    private List<Booking> bookingList = new ArrayList<Booking>();
+    private int currentId = 1;
+
+    public List<Booking> getAllBookings() {
+        return bookingList;
+    }
+    public void addBooking(Booking booking) {
+        booking.setBookingId(currentId++);
+        bookingList.add(booking);
+    }
+
+    public void updateBooking(int bookingId, String roomType) {
+        for (Booking booking : bookingList) {
+            if (booking.getBookingId() == bookingId) {
+                booking.setRoomType(roomType);
+                System.out.println("Booking details are updated");
+                return;
+            }
+        }
+        System.out.println("Booking not found");
+    }
+
+    public void deleteBooking(int bookingId) {
+        for (Booking booking : bookingList) {
+            if (booking.getBookingId() == bookingId) {
+                bookingList.remove(booking);
+                System.out.println("Booking details are deleted");
+                return;
+            }
+        }
+        System.out.println("Booking not found");
+    }
 }
