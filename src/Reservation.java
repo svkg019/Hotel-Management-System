fba3ca9aa9e72dbcdd41a4feebf37928da392626
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-16 21:11:50 +0530

    add class members in the Reservation class

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Reservation {
+    private int reservationId;
+    private String checkInDate;
+    private String checkOutDate;
+    private String roomPreference;
+    private String name;
+    private String contact;
+
+    public Reservation(int reservationId, String checkInDate, String checkOutDate, String roomPreference, String name, String contact) {
+        this.reservationId = reservationId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.roomPreference = roomPreference;
+        this.name = name;
+        this.contact = contact;
+    }
 }
