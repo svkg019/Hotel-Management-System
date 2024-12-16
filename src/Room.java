9b8b69a5b0e3737c67fc043cfd6abfa74bd039e6
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-16 21:26:36 +0530

    add class members in the Room class

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,13 @@
 public class Room {
+  private String roomType;
+    private int dateOfAvailability;
+    private int price;
+    private String place;
+
+    public Room(String roomType, int dateOfAvailability, int price, String place) {
+        this.roomType = roomType;
+        this.dateOfAvailability = dateOfAvailability;
+        this.price = price;
+        this.place = place;
+    }
 }
