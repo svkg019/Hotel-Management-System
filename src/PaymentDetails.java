fd4e34b09b9fa2ccac04d5e134d3c3d23f5df138
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-16 21:30:01 +0530

    add class members in the PaymentDetails class

--- src/PaymentDetails.java
+++ src/PaymentDetails.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class PaymentDetails {
+    private String cardHolderName;
+    private int cardNumber;
+    private int cvv;
+    private String expiryDate;
+
+    public PaymentDetails(String cardHolderName, int cardNumber, int cvv, String expiryDate) {
+        this.cardHolderName = cardHolderName;
+        this.cardNumber = cardNumber;
+        this.cvv = cvv;
+        this.expiryDate = expiryDate;
+    }
 }
