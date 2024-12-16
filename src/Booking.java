a242f8fb009ce3fc1fecc8bb2118c01745222331
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-16 21:34:14 +0530

    add class members in the Booking class

--- src/Booking.java
+++ src/Booking.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Booking {
+    private int bookingId;
+    private String roomType;
+    private String bookingDetails;
+    private double price;
+
+    public Booking(int bookingId, String roomType, String bookingDetails, double price) {
+        this.bookingId = bookingId;
+        this.roomType = roomType;
+        this.bookingDetails = bookingDetails;
+        this.price = price;
+    }
 }
