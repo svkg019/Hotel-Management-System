79d003d5907ab0e6ed5d7a37ded893f2334ef16d
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-16 21:09:26 +0530

    add class members in the user class

--- src/User.java
+++ src/User.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class User {
+    private int userId;
+    private String fullName;
+    private String email;
+    private int phoneNumber;
+    private String password;
+
+    public User(int userId, String fullName, String email, int phoneNumber, String password) {
+        this.userId = userId;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
 }
