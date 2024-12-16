25c12e7948cb2bd99454ba7cea6563396a99ede3
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-16 21:18:55 +0530

    add class members in the Feedback class

--- src/Feedback.java
+++ src/Feedback.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Feedback {
+    private String userName;
+    private int contactNumber;
+    private int RoomNumber;
+    private String typeOfComplaint;
+    private String feedback;
+
+    public Feedback(String userName, int contactNumber, int RoomNumber, String typeOfComplaint, String feedback) {
+        this.userName = userName;
+        this.contactNumber = contactNumber;
+        this.RoomNumber = RoomNumber;
+        this.typeOfComplaint = typeOfComplaint;
+        this.feedback = feedback;
+    }
 }
