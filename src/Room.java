3f5dbc532c55547bf018ea6f76fddf21bdb4f3ac
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-17 22:07:52 +0530

    add variable isBooked with getter and setter

--- src/Room.java
+++ src/Room.java
@@ -1,16 +1,21 @@
 public class Room {
-  private String roomType;
+    private boolean isBooked;
+    private String roomType;
     private int dateOfAvailability;
     private int price;
     private String place;
 
-    public Room(String roomType, int dateOfAvailability, int price, String place) {
+    public Room(boolean isBooked, String roomType, int dateOfAvailability, int price, String place) {
+        this.isBooked = isBooked;
         this.roomType = roomType;
         this.dateOfAvailability = dateOfAvailability;
         this.price = price;
         this.place = place;
     }
 
+    public boolean getIsBooked() {
+        return isBooked;
+    }
     public String getRoomType() {
         return roomType;
     }
@@ -24,7 +29,9 @@ public class Room {
         return place;
     }
 
-
+    public void setIsBooked(boolean isBooked) {
+        this.isBooked = isBooked;
+    }
     public void setRoomType(String roomType) {
         this.roomType = roomType;
     }
