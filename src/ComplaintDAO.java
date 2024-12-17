cd16414472f7d8015139c580ee9ff9c08624fa40
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-17 21:59:08 +0530

    add complaintDao class with necessary DB and functions

---
+++ src/ComplaintDAO.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ComplaintDAO {
+    private List<Complaint> complaintList = new ArrayList<>();
+
+    public List<Complaint> getAllComplaints() {
+        return complaintList;
+    }
+    public void addComplaint(Complaint complaint) {
+        complaintList.add(complaint);
+        System.out.println("Complaint Registered Successfully");
+    }
+}
