eeda4cb14cd0d60fbf14c47386300f09db3255c9
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-17 23:21:04 +0530

    add user profile related opperations

--- src/HotelService.java
+++ src/HotelService.java
@@ -1,2 +1,25 @@
+import java.util.*;
 public class HotelService {
+    private final UserDAO userDAO;
+
+    public HotelService() {
+        userDAO = new UserDAO();
+    }
+
+    public void registerUser(User user) {
+        userDAO.addUser(user);
+    }
+    public void updateUser(int userId, String email) {
+        userDAO.updateUser(userId, email);
+    }
+    public void deleteUser(int userId) {
+        userDAO.deleteUser(userId);
+    }
+    public void viewUsers() {
+        List<User> users = userDAO.getAllUsers();
+        for (User user : users) {
+            System.out.println(user.getUserId() + " " + user.getFullName() + " " + user.getEmail() + " " + user.getPhoneNumber());
+        }
+    }
+
 }
