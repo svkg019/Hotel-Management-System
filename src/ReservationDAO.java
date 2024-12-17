48ce0ca907cf1567f34d3f3d3d0b10cf42a58d3a
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-17 21:49:52 +0530

    add bookingHistory for customer

--- src/ReservationDAO.java
+++ src/ReservationDAO.java
@@ -2,26 +2,37 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class ReservationDAO {
+    //Reservation List for Admin
     private List<Reservation> reservationList = new ArrayList<Reservation>();
+    //Boooking History for User
+    private List<Reservation> bookingHistory = new ArrayList<Reservation>();
     private int currentId = 1;
 
     public List<Reservation> getAllReservations() {
         return reservationList;
     }
+    public List<Reservation> getAllBookingHistory() {
+        return bookingHistory;
+    }
 
     public void addReservation(Reservation reservation) {
         reservation.setReservationId(currentId++);
         reservationList.add(reservation);
+        bookingHistory.add(reservation);
         System.out.println("Reservation Successful");
     }
 
     public void updateReservation(int reservationId, String name) {
-        for (Reservation reservation : reservationList) {
+        int i = 0;
+        while (i < reservationList.size()) {
+            Reservation reservation = reservationList.get(i);
             if (reservation.getReservationId() == reservationId) {
                 reservation.setName(name);
-                System.out.println("User details are updated sucessfully");
+                bookingHistory.set(i, reservation);
+                System.out.println("User details are updated successfully");
                 return;
             }
+            i++;
         }
         System.out.println("Reservation not found");
     }
