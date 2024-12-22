4c6032e6385e642caa77fc76c49e1adb91fa8ffe
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-22 14:21:00 +0530

    add getter and setter methods in PaymentDetails class

--- src/PaymentDetails.java
+++ src/PaymentDetails.java
@@ -12,4 +12,38 @@ public class PaymentDetails {
         this.cvv = cvv;
         this.expiryDate = expiryDate;
     }
+    //getter
+    public String getUserId() {
+        return userId;
+    }
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+    public int getCardNumber() {
+        return cardNumber;
+    }
+    public int getCvv() {
+        return cvv;
+    }
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+    //setter
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+
 }
