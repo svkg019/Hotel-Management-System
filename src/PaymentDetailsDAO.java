594fa0b9c43e8fed23904204e7c6e62fa4e95198
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-22 14:16:05 +0530

    update PaymentDetailsDAO class

--- src/PaymentDetailsDAO.java
+++ src/PaymentDetailsDAO.java
@@ -1,3 +1,16 @@
-public class PaymentDetailsDAO {
+import java.util.List;
+import java.util.ArrayList;
 
+
+public class PaymentDetailsDAO {
+    List<PaymentDetails> paymentDetailsList = new ArrayList<>();
+
+
+    public void addPaymentDetails(PaymentDetails pd) {
+        paymentDetailsList.add(pd);
+    }
+
+    public List<PaymentDetails> getAllBookings() {
+        return paymentDetailsList;
+    }
 }
