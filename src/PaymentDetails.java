87bc1a484e71aa2a7aca819a58ec718973a89d43
Souvik Ghosh <souvikghosh.education@gmail.com>
2024-12-22 14:18:43 +0530

    add userID variable in PaymentDetails class

--- src/PaymentDetails.java
+++ src/PaymentDetails.java
@@ -1,10 +1,12 @@
 public class PaymentDetails {
+    private String userId;
     private String cardHolderName;
     private int cardNumber;
     private int cvv;
     private String expiryDate;
 
-    public PaymentDetails(String cardHolderName, int cardNumber, int cvv, String expiryDate) {
+    public PaymentDetails(String userId, String cardHolderName, int cardNumber, int cvv, String expiryDate) {
+        this.userId = userId;
         this.cardHolderName = cardHolderName;
         this.cardNumber = cardNumber;
         this.cvv = cvv;
